56d1f46adc9456717ca7a25cfec5db27e45f4ace
Bruno Queiroz <Bruno.Queiroz@mv.com.br>
2019-12-05 17:34:30 -0300

    Ajuste no Dismissible para excluir e editar e no icone do app

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:path_provider/path_provider.dart';
 
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
@@ -17,11 +18,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
-
+ 
   final _toDoController =TextEditingController();
 
   List _toDoList = [];
+
+  bool _isEdit = false;
+  int _todoEditPos;
 
   Map<String, dynamic> _lastRemoved;
   int _lastRemovedPos;
@@ -38,14 +41,21 @@ class _HomeState extends State<Home> {
   }
 
   void _addToDO(){
-    setState(() {
-      Map<String, dynamic> newToDo = Map();
-      newToDo["title"] =_toDoController.text;
-      _toDoController.text = "";
-      newToDo["ok"] = false;
-      _toDoList.add(newToDo);
+    setState(() {     
+      if (!_isEdit) { 
+        Map<String, dynamic> newToDo = Map();
+        newToDo["title"] =_toDoController.text;
+        _toDoController.text = "";               
+        newToDo["ok"] = false;
+        _toDoList.add(newToDo);
 
-      _saveData();
+        _saveData();
+      } else {
+        _toDoList[_todoEditPos]["title"] = _toDoController.text;
+        _toDoController.text = "";
+        _isEdit = false;        
+        _saveData();
+      }      
     });
   }
 
@@ -111,17 +121,70 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget _slideRightBackground(){
+    return Container(
+      color: Colors.green,
+      child: Align(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              Icons.edit,
+              color: Colors.white
+            ),
+            Text(
+              " Editar",
+              style:TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700
+              ),
+              textAlign: TextAlign.left,
+            )
+          ],
+        ),
+        alignment: Alignment.centerLeft,
+      ),
+    );
+  }
+
+  Widget _slideLeftBackground(){
+    return Container(
+      color: Colors.red,
+      child: Align(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Icon(
+              Icons.delete,
+              color: Colors.white,              
+            ),
+            Text(
+              " Deletar",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700
+              ),
+              textAlign: TextAlign.right,
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+        ),
+        alignment: Alignment.centerRight,
+      ),
+    );
+  }
+
   Widget buildItem(BuildContext context, int index){
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-      background: Container(
-        color: Colors.red,
-        child: Align(
-          alignment: Alignment(-0.9, 0.0),
-          child: Icon(Icons.delete, color: Colors.white,),
-        ),
-      ),
-      direction: DismissDirection.startToEnd,
+      background: _slideRightBackground(),
+      secondaryBackground: _slideLeftBackground(),
+      //direction: DismissDirection.startToEnd,
       child: CheckboxListTile(
         title: Text(_toDoList[index]["title"]),
         value: _toDoList[index]["ok"],
@@ -136,29 +199,37 @@ class _HomeState extends State<Home> {
         },
       ),
       onDismissed: (direction){
-        setState(() {
-          _lastRemoved = Map.from(_toDoList[index]);
-          _lastRemovedPos = index;
-          _toDoList.removeAt(index);
+        if (direction ==DismissDirection.endToStart) {
+          setState(() {
+            _lastRemoved = Map.from(_toDoList[index]);
+            _lastRemovedPos = index;
+            _toDoList.removeAt(index);
 
-          _saveData();
+            _saveData();
 
-          final snack = SnackBar(
-            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
-            action: SnackBarAction(label: "Desfazer",
-                onPressed: () {
-                  setState(() {
-                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
-                    _saveData();
-                  });
-                }),
-            duration: Duration(seconds: 2),
-          );
+            final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+              action: SnackBarAction(label: "Desfazer",
+                  onPressed: () {
+                    setState(() {
+                      _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                      _saveData();
+                    });
+                  }),
+              duration: Duration(seconds: 2),
+            );
 
-          Scaffold.of(context).removeCurrentSnackBar();
-          Scaffold.of(context).showSnackBar(snack);
+            Scaffold.of(context).removeCurrentSnackBar();
+            Scaffold.of(context).showSnackBar(snack);
 
-        });
+          }); 
+        }else{
+          setState(() {  
+            _todoEditPos = index;
+            _isEdit =true;
+            _toDoController.text =  _toDoList[index]["title"];
+          });
+        }        
       },
     );
   }
