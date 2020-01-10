8b1a4bfc072da48541cbea8ddd94999e5be9ce30
Bruno Queiroz <Bruno.Queiroz@mv.com.br>
2020-01-10 18:41:03 -0300

    Projeto de agenda de contato

---
+++ contact_schedule/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:contact_schedule/UIs/contact_page.dart';
+import 'package:contact_schedule/UIs/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
